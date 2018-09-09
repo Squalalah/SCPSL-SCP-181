2da2e7a517f4c2d6324de4d3bd3af4ba3e7ccd6e
anthony montreuil <montreuil.anthony@gmail.com>
2018-09-09 17:26:10 +0200

    Update to 3.1.15 smod2

--- SCP181Event/SCP181Event.cs
+++ SCP181Event/SCP181Event.cs
@@ -13,10 +13,10 @@ namespace SCP181
         name = "SCP-181",
         description = "Un Classe-D aléatoire devient SCP-181 et défie les lois de la probabilité.",
         id = "squal.181.event",
-        version = "1.3.1",
+        version = "1.3.2",
         SmodMajor = 3,
         SmodMinor = 1,
-        SmodRevision = 12
+        SmodRevision = 15
         )]
 
     /////////////////////////////////////////////////////////////////////////////////////////////
